c5eb99d30d24da491f55c545b2473b2977af8f78
XyLigaN4eG777 <92299169+XyLigaN4eG777@users.noreply.github.com>
2022-03-28 06:16:23 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,153 +1,279 @@
 #include <iostream>
-#include <vector>
+#include <SDL.h>
+#include <conio.h>
+#include<vector>
+#define WINDOW_WIDTH 1000
 using namespace std;
 
-int absi(int a) {
-    if (a < 0)
-        return a * (-1);
-    return a;
-}
 
-long long itc_len(string str) {
-    long long res = 0;
-    int i = 0;
-    while (str[i] != '\0') {
-        i++;
+void verticalLine(SDL_Renderer* render, int x, int y, int length) {
+    bool side = false;
+    if (length < 0) {
+        side = true;
+        length *= -1;
     }
-    return i;
-}
-
-string slicer(string str, int start, int endd) {
-    string res = "";
-    if (endd >= itc_len(str)) {
-        endd = itc_len(str) - 1;
-    }
-    if (start > endd) {
-        return str;
-    }
-    else if (start == (itc_len(str) - 1)) {
-        res += str[itc_len(str) - 1];
-        return res;
-    }
-    else {
-        for (long long i = start; i <= endd; i++) {
-            res += str[i];
-        }
-        return res;
-    }
-
-    return "-1";
-}
-
-bool isnum(string s) {
-    for (int i = 0; i < itc_len(s); i++) {
-        if (s[i] < '0' && s[i] > '9') return false;
-    }
-    return true;
-}
-
-long long str_to_num(string temp) {
-    int ch = 0;
-    long long temp_num = 0;
-    for (long long i = 0; temp[i] != '\0'; i++) {
-        ch = temp[i] - 48;
-        temp_num = temp_num * 10 + ch;
-    }
-    return temp_num;
-}
-
-
-void background(vector<vector<char>>& field, int width, int height, char background_char) {
-    for (int i = 0;i < height; i++) {
-        for (int j = 0; j < width; j++) {
-            field[i][j] = background_char;
-        }
+    for (int i = 0; i < length; i++) {
+        SDL_RenderDrawPoint(render, x, y);
+        if (side)
+            y += 1;
+        else
+            y -= 1;
     }
 }
 
 
-void r(vector<vector<char>>& field, int x, int y, int width, int height, char ch) {
-    for (int i = 0; i < height; i++) {
-        for (int j = 0; j < width; j++) {
-            if (i == 0) {
-                field[y][x + j] = ch;
-            }
-            else if (i = height - 1) {
-                field[y + i][x + j] = ch;
-            }
-            else if (j == 0) {
-                field[y + i][x] = ch;
-            }
-            else if (j == width - 1) {
-                field[y + i][x + j] = ch;
-            }
-        }
+void rect(SDL_Renderer* render, int x, int y, int h, int w) {
+    h *= (-1);
+    for (int i = 0; i < w; i++) {
+        verticalLine(render, x, y, h);
+        x += 1;
     }
 }
 
+void menu(SDL_Renderer* renderer) {
+    rect(renderer, 190, 100, 20, 100);
+    rect(renderer, 230, 115, 125, 20);
+
+    rect(renderer, 370, 100, 25, 40);
+    rect(renderer, 355, 125, 25, 20);
+    rect(renderer, 405, 125, 25, 20);
+    rect(renderer, 340, 150, 90, 20);
+    rect(renderer, 420, 150, 90, 20);
+    rect(renderer, 340, 175, 20, 100);
+
+    rect(renderer, 490, 100, 140, 20);
+    rect(renderer, 570, 100, 140, 20);
+    rect(renderer, 510, 120, 20, 12);
+    rect(renderer, 522, 140, 20, 12);
+    rect(renderer, 534, 160, 20, 12);
+    rect(renderer, 546, 180, 20, 12);
+    rect(renderer, 558, 200, 20, 12);
+
+    rect(renderer, 640, 100, 140, 20);
+    rect(renderer, 720, 100, 20, 20);
+    rect(renderer, 700, 120, 20, 20);
+    rect(renderer, 680, 140, 20, 20);
+    rect(renderer, 660, 160, 20, 20);
+    rect(renderer, 680, 180, 20, 20);
+    rect(renderer, 700, 200, 20, 20);
+    rect(renderer, 720, 220, 20, 20);
+
+    rect(renderer, 790, 100, 20, 20);
+    rect(renderer, 790, 150, 90, 20);
+
+    rect(renderer, 360, 300, 20, 20);
+    rect(renderer, 440, 300, 20, 20);
+    rect(renderer, 380, 280, 20, 60);
+    rect(renderer, 440, 320, 20, 20);
+    rect(renderer, 420, 340, 20, 20);
+    rect(renderer, 400, 360, 20, 20);
+    rect(renderer, 390, 380, 20, 20);
+    rect(renderer, 370, 400, 20, 100);
+
+    rect(renderer, 490, 340, 20, 60);
+
+    rect(renderer, 570, 280, 140, 20);
+    rect(renderer, 630, 320, 60, 20);
+    rect(renderer, 590, 280, 20, 20);
+    rect(renderer, 610, 300, 20, 20);
+    rect(renderer, 590, 400, 20, 20);
+    rect(renderer, 610, 380, 20, 20);
 
 
-void R(vector<vector<char>>& field, int x, int y, int width, int height, char ch) {
-    for (int i = 0; i < height; i++) {
-        for (int j = 0; j < width; j++) {
-            field[y + i][x + j] = ch;
-        }
-    }
+    rect(renderer, 100, 600, 70, 10);
+    rect(renderer, 140, 610, 30, 10);
+    rect(renderer, 110, 600, 10, 30);
+    rect(renderer, 110, 640, 10, 30);
+
+    rect(renderer, 160, 600, 70, 10);
+
+    rect(renderer, 195, 600, 10, 20);
+    rect(renderer, 190, 610, 10, 15);
+    rect(renderer, 205, 610, 10, 15);
+    rect(renderer, 180, 620, 50, 10);
+    rect(renderer, 220, 620, 50, 10);
+    rect(renderer, 180, 640, 10, 50);
+
+    rect(renderer, 240, 600, 10, 10);
+    rect(renderer, 280, 600, 10, 10);
+    rect(renderer, 250, 610, 10, 10);
+    rect(renderer, 270, 610, 10, 10);
+    rect(renderer, 260, 620, 50, 10);
+
+    rect(renderer, 300, 600, 70, 10);
+    rect(renderer, 300, 600, 10, 40);
+    rect(renderer, 300, 640, 10, 40);
+    rect(renderer, 300, 660, 10, 40);
+
+    rect(renderer, 360, 600, 70, 10);
+    rect(renderer, 370, 640, 10, 10);
+    rect(renderer, 380, 650, 10, 10);
+    rect(renderer, 380, 660, 10, 10);
+    rect(renderer, 370, 600, 10, 10);
+    rect(renderer, 380, 610, 20, 10);
+    rect(renderer, 370, 630, 10, 10);
+
+    rect(renderer, 420, 600, 10, 50);
+    rect(renderer, 420, 660, 10, 50);
+    rect(renderer, 440, 600, 70, 10);
+
+    rect(renderer, 550, 600, 70, 10);
+    rect(renderer, 590, 610, 30, 10);
+    rect(renderer, 560, 600, 10, 30);
+    rect(renderer, 560, 640, 10, 30);
+
+    rect(renderer, 610, 600, 70, 10);
+
+    rect(renderer, 645, 600, 10, 20);
+    rect(renderer, 640, 610, 10, 15);
+    rect(renderer, 655, 610, 10, 15);
+    rect(renderer, 630, 620, 50, 10);
+    rect(renderer, 670, 620, 50, 10);
+    rect(renderer, 630, 640, 10, 50);
+
+    rect(renderer, 690, 600, 10, 10);
+    rect(renderer, 730, 600, 10, 10);
+    rect(renderer, 700, 610, 10, 10);
+    rect(renderer, 720, 610, 10, 10);
+    rect(renderer, 710, 620, 50, 10);
+
+    rect(renderer, 750, 600, 70, 10);
+    rect(renderer, 750, 600, 10, 40);
+    rect(renderer, 750, 640, 10, 40);
+    rect(renderer, 750, 660, 10, 40);
+
+    rect(renderer, 810, 600, 70, 10);
+    rect(renderer, 820, 640, 10, 10);
+    rect(renderer, 830, 650, 10, 10);
+    rect(renderer, 830, 660, 10, 10);
+    rect(renderer, 820, 600, 10, 10);
+    rect(renderer, 830, 610, 20, 10);
+    rect(renderer, 820, 630, 10, 10);
+
+    rect(renderer, 870, 600, 10, 50);
+    rect(renderer, 870, 660, 10, 50);
+    rect(renderer, 880, 600, 70, 10);
+    rect(renderer, 900, 600, 70, 10);
+    //keys 1
+    rect(renderer, 200, 700, 5, 100);
+    rect(renderer, 200, 800, 5, 100);
+    rect(renderer, 200, 700, 100, 5);
+    rect(renderer, 300, 700, 105, 5);
+    rect(renderer, 225, 720, 60, 10);
+    rect(renderer, 265, 720, 60, 10);
+    rect(renderer, 235, 780, 10, 10);
+    rect(renderer, 255, 780, 10, 10);
+    rect(renderer, 245, 740, 50, 10);
+
+    rect(renderer, 100, 800, 5, 100);
+    rect(renderer, 100, 900, 5, 100);
+    rect(renderer, 100, 800, 100, 5);
+    rect(renderer, 200, 800, 105, 5);
+    rect(renderer, 130, 820, 10, 20);
+    rect(renderer, 140, 820, 10, 20);
+    rect(renderer, 140, 810, 10, 40);
+    rect(renderer, 110, 850, 10, 80);
+    rect(renderer, 130, 820, 60, 10);
+    rect(renderer, 170, 820, 60, 10);
+
+    rect(renderer, 200, 800, 5, 100);
+    rect(renderer, 200, 900, 5, 100);
+    rect(renderer, 200, 800, 100, 5);
+    rect(renderer, 300, 800, 105, 5);
+    rect(renderer, 230, 810, 10, 40);
+    rect(renderer, 270, 820, 10, 10);
+    rect(renderer, 220, 820, 10, 10);
+    rect(renderer, 220, 830, 10, 10);
+    rect(renderer, 230, 840, 10, 40);
+    rect(renderer, 270, 850, 20, 10);
+    rect(renderer, 220, 860, 10, 10);
+    rect(renderer, 230, 870, 10, 40);
+
+    rect(renderer, 300, 800, 5, 100);
+    rect(renderer, 300, 900, 5, 100);
+    rect(renderer, 300, 800, 100, 5);
+    rect(renderer, 400, 800, 105, 5);
+    rect(renderer, 320, 810, 10, 50);
+    rect(renderer, 320, 810, 80, 10);
+    rect(renderer, 320, 880, 10, 50);
+    rect(renderer, 370, 820, 70, 10);
+
+    // keys 2
+    rect(renderer, 700, 700, 5, 100);
+    rect(renderer, 700, 800, 5, 100);
+    rect(renderer, 700, 700, 100, 5);
+    rect(renderer, 800, 700, 105, 5);
+    rect(renderer, 745, 720, 60, 10);
+    rect(renderer, 740, 720, 10, 10);
+    rect(renderer, 750, 720, 10, 10);
+    rect(renderer, 735, 730, 10, 10);
+    rect(renderer, 755, 730, 10, 10);
+    rect(renderer, 745, 710, 10, 10);
+
+    rect(renderer, 600, 800, 5, 100);
+    rect(renderer, 600, 900, 5, 100);
+    rect(renderer, 600, 800, 100, 5);
+    rect(renderer, 700, 800, 105, 5);
+    rect(renderer, 620, 845, 10, 60);
+    rect(renderer, 630, 835, 30, 10);
+    rect(renderer, 640, 830, 40, 10);
+   
+    rect(renderer, 700, 800, 5, 100);
+    rect(renderer, 700, 900, 5, 100);
+    rect(renderer, 700, 800, 100, 5);
+    rect(renderer, 800, 800, 105, 5);
+    rect(renderer, 745, 810, 60, 10);
+    rect(renderer, 735, 850, 10, 30);
+    rect(renderer, 730, 840, 10, 40);
+
+    rect(renderer, 800, 800, 5, 100);
+    rect(renderer, 800, 900, 5, 100);
+    rect(renderer, 800, 800, 100, 5);
+    rect(renderer, 900, 800, 105, 5);
+    rect(renderer, 830, 845, 10, 60);
+    rect(renderer, 870, 840, 20, 10);
+    rect(renderer, 860, 835, 30, 10);
+
 }
 
-
-void c(vector<vector<char>>& field, int x0, int y0, int rad, char ch) {
-    int x, y = rad;
+void pole(SDL_Renderer* renderer) {
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
+    SDL_RenderClear(renderer);
+    SDL_SetRenderDrawColor(renderer, 165, 30, 0, 0);
+    rect(renderer, 200, 200, 250, 50);
+    rect(renderer, 200, 550, 250, 50);
+    rect(renderer, 400, 200, 250, 50);
+    rect(renderer, 400, 200, 250, 50);
+    SDL_RenderPresent(renderer);
+    SDL_RenderClear(renderer);
 }
 
-void line_h(vector<vector<char>>& field, int x, int y, int len, char ch) {
-    for (int i = 0; i < len; i++) {
-        field[y][x + i] = ch;
-    }
-}
-
-void line_v(vector<vector<char>> &field, int x, int y, int len, char ch) {
-    for (int i = 0; i < len; i++) {
-        field[y + i][x] = ch;
-    }
-}
-
-void L(vector<vector<char>> &field, int x0, int y0, int x1, int y1, char ch) {
-    int dy = y1 - y0;
-    int dx = x1 - x0;
-    if (dy == 0) line_h(field, x0, y0, absi(dx), ch);
-    if (dx == 0) line_v(field, x0, y0, absi(dy), ch);
-    else {
-        int signy = 1, signx = 1;
-        int f = 0;
-        if (dy < 0) signy = -1;
-        if (dx < 0) signx = -1;
-        field[y0][x0] = ch;
-        while (x0 != x1 && y0 != y1) {
-            if (absi(dy) < absi(dx)) {
-                f = f + dy * signy;
-                if (f > 0) {
-                    f = f - dx * signx;
-                    y0 = y0 + signy;
-                }
-
-                x0 = x0 - signx;
-                field[y0][x0] = ch;
-            }
-            else {
-                f = f + dx * signx;
-                if (f > 0) {
-                    f = f - dy * signy;
-                    x0 = x0 - signx;
-                }
-                y0 = y0 + signy;
-                field[y0][x0] = ch;
-            }
-        }
-    }
-}
-int main()
+int main(int argc, char* args[])
 {
-    cout << "Hello world!" << endl;
+    SDL_Renderer* renderer;
+    SDL_Window* window;
+    SDL_Init(SDL_INIT_VIDEO);
+    SDL_CreateWindowAndRenderer(WINDOW_WIDTH, WINDOW_WIDTH, 0, &window, &renderer);
+    SDL_SetRenderDrawColor(renderer, 116, 78, 59, 0);
+    SDL_RenderClear(renderer);
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
+    menu(renderer);
+    SDL_RenderPresent(renderer);
+    SDL_RenderClear(renderer);
+    char ch;
+    int k = 0;
+    int code;
+    while (k != 1)
+    {
+        ch = _getch();
+        code = static_cast<int>(ch);
+        if (ch == 13) 
+            k = 1;
+    }
+    pole(renderer);
+    SDL_Delay(30000);
+    SDL_Quit();
     return 0;
 }
+
+ 
